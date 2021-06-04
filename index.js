48b6b07b78f09176e5c95d4825ed34ae41e7a0ec
Marcos Cury Moreira <quinhopd@gmail.com>
2021-06-04 01:20:46 -0300

    updating

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/produto', (req, res) => {
 
 //DESAFIO 2 - GET POR ID - OK
 app.get('/produto/:id', (req, res) => {
-    let {id} = req.params;
+    let id = req.params.id;
     let produtocorreto = 0;
     for (listaBusca = 0; listaBusca < listaProdutos.length; listaBusca++){
         if (id == listaProdutos[listaBusca].idProduto){
@@ -42,40 +42,31 @@ app.get('/produto/:id', (req, res) => {
 app.post('/produto', (req, res) => {
     let {idProduto, descricao, preco, qtdEstoque, disponivel, emDestaque, departamento, idDepto, nomeDepto} = req.body
 
-    if (idProduto < 0 || !descricao || preco < 0 || qtdEstoque < 0 || !disponivel || !emDestaque || !departamento){
+    if (idProduto <= 0 || !descricao || preco <= 0 || qtdEstoque < 0 || !disponivel || !emDestaque || !departamento  || departamento.idDepto <= 0  || !departamento.nomeDepto || idProduto === listaProdutos.idProduto){
         res.sendStatus(400);
     }
-
-    let novoProduto = {
-        idProduto,
-        descricao,
-        preco,
-        qtdEstoque,
-        disponivel,
-        emDestaque,
-        departamento,
-        idDepto,
-        nomeDepto
-    };
-
-    listaProdutos.push(novoProduto)
-
-    res.json(novoProduto)
+    else{
+        let novoProduto = {
+            idProduto,
+            descricao,
+            preco,
+            qtdEstoque,
+            disponivel,
+            emDestaque,
+            departamento,
+            idDepto,
+            nomeDepto
+        };
     
+        listaProdutos.push(novoProduto)
+    
+        res.json(novoProduto)
+    }
 
-    // if(produto.idProduto < 0 || !produto.descricao || !produto.disponivel || produto.preco <= 0){
-    //     //res.sendStatus(400)
-    //     res.status(400).json({message: 'Parâmetros incorretos. Verifique as informações inseridas e tente novamente.'})
-    // }
-    // else{
-    //     listaProdutos.push(produto);
-    //     //res.sendStatus(200)
-    //     res.status(200).json({message: 'Produto incluído na base de dados.'})
-    // }
+
 })
 
-
-//DESAFIO 3 ISIDRO
+//DESAFIO 3 - ISIDRO
 // app.post("/produto", (req, res) => {
 //     let produto = req.body;
     
@@ -89,15 +80,15 @@ app.post('/produto', (req, res) => {
     
 // })
 
-// app.listen(3000, () => {
-//     console.log("Servidor no ar na porta 3000 - vamos ver se funfa!!!");
-// })
+
+
 
 
 
 //DESAFIO 4 PUT - INCOMPLETO
 app.put('/produto/:id', (req, res) => {
     let {id} = req.params;
+
     let produto = listaProdutos.find(prod => prod.id == id);
     if (!produto) return res.status(204).json();
 
@@ -116,7 +107,28 @@ app.put('/produto/:id', (req, res) => {
 
 //DESAFIO 5 GET DEPARTAMENTO - INCOMPLETO
 app.get('/departamento', (req, res) => {
-    let produto = listaProdutos.departamento
+    let idDepto = 0
+    let departamento = {
+        id: 0,
+        nomeDepto: "",
+    }
+    
+    for(pos=0; pos<listaProdutos[pos].departamento.idDepto; pos++){
+        if(idDepto != listaProdutos[pos].departamento.idDepto){
+             if (departamento.id != 0){
+                 console.log("Departamento: " + departamento.nomeDepto);
+                 console.log("");
+             }
+             idDepto = listaProdutos[pos].departamento.idDepto;
+             departamento.id = idDepto;
+             departamento.nomeDepto = listaProdutos[pos].departamento.nomeDepto;
+
+        }
+
+     }
+     console.log("Departamento: " + departamento.nomeDepto);
+     console.log("Id departamento: " + departamento.idDepto);
+    let produto = listaProdutos.departamento.nomeDepto
     res.send(produto)
 })
 
