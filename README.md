209e41fa5000534a805fe6972916f6e6b68a0642
Marcos Cury Moreira <quinhopd@gmail.com>
2021-06-03 23:10:07 -0300

    updating

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# Loading...
+# O que fizemos?
+Trata-se de uma RESP API para um e-commerce. Utilizando os métodos e arquitetura REST, desenvolvemos um projeto para apresentar todo o estoque do e-commerce, buscar um produto específico, incluir novos produtos e alterar dados de produtos existentes.
+
+# O que utilizamos?
+* NodeJS v14.17.0
+* Express 4.17.1
+
+# E aí, como eu uso?
+* Link para clonar o projeto: [GitHub](https://github.com/marcoscurymoreira/ecommerce-api-gama.git)
+  
+* Algumas linhas de teste para utilizarmos:
+  * Utilizando o método GET para retornar toda a lista de produtos: http://localhost:3000/produto
+  
+    * Detalhes adicionais: deverá retornar toda a lista de produtos e CODE 200.
+  
+  * Utilizando o método GET para retornar um item da lista de produtos: http://localhost:3000/produto/{id}
+  
+    * Detalhes adicionais: as IDs válidas são do 1 ao 10, qualquer outra ID deverá retornar Code 404. Caso o ID esteja correto, deverá retornar o produto referente e CODE 200.
+  
+  * Utilizando o método POST para incluir um novo produto na base de dados: http://localhost:3000/produto
+  
+    * Detalhes adicionais: todos os dados deverão ser preenchidos para a correta inclusão do novo produto na base de dados, caso contrário, deverá retornar Code 400. Caso esteja tudo correto, deverá retornar o objeto inserido e CODE 200.
+  
+  * Utilizando o método PUT para alterar um dado na nossa lista de produtos: http://localhost:3000/produto/{id}
+  
+    * Detalhes adicionais: caso o ID informado seja inexistente em nossa base de dados, deverá retornar CODE 404. Se for passado algum objeto incompleto ou zerado, deverá retornar CODE 400. Caso dê tudo certo, deverá retornar o próprio objeto e CODE 200.
+  
+  * Utilizando o método GET para retornar toda a lista de departamentos: http://localhost:3000/departamento 
+  
+    * Detalhes adicionais: deverá retornar toda a lista de departamentos e CODE 200.
+  
+  * Utilizando o método GET para retornar toda a lista de produtos de um determinado departamento: http://localhost:3000/departamento/{id} 
+  
+    * Detalhes adicionais: caso o ID do departamento informado seja inexistente em nossa base de dados, deverá retornar CODE 404. Caso dê tudo certo, deverá retornar a lista de produtos daquele departamento e CODE 200.
+
+# O que mais preciso saber?
+* Este é um projeto realizado durante o Gama XP37. Todos os dados utilizados são fictícios.
+
+# Quem somos?
+* Anderley Quinteiro
+* [Marcos Moreira](https://github.com/marcoscurymoreira)
+* Victor Reginato
--- index.js
+++ index.js
@@ -1,178 +1,138 @@
-//inclusão dos pacotes
 const express = require('express')
-
-//instanciando o express
 const app = express()
-
-//definindo as portas
+const listaProdutos = require('./listaProdutos.json')
 const port = 3000
 
-//lista de produtos
-var listaProdutos = [
-    {
-        idProduto: 1,
-        descricao: "ADAPTADOR BLUETOOH USB RECEPTOR DE AUDIO P2",
-        preco: 5.0,
-        qtdEstoque: 10,
-        disponivel: "sim",
-        emDestaque: "sim",
-        departamento: {
-            idDepto: 1,
-            nomeDepto: "Adaptadores"
-        }
-    },
-    {
-        idProduto: 2,
-        descricao: "ADAPTADOR CONECTOR HDMI FEMEA L / FEMEA",
-        preco: 2.8,
-        qtdEstoque: 20,
-        disponivel: "sim",
-        emDestaque: "sim",
-        departamento: {
-            idDepto: 1,
-            nomeDepto: "Adaptadores"
-        }
-    },
-    {
-        idProduto: 3,
-        descricao: "ADAPTADOR CONECTOR HDMI MACHO L / FEMEA",
-        preco: 3.5,
-        qtdEstoque: 10,
-        disponivel: "sim",
-        emDestaque: "nao",
-        departamento: {
-            idDepto: 1,
-            nomeDepto: "Adaptadores"
-        }
-    },
-    {
-        idProduto: 4,
-        descricao: "ADAPTADOR CONVERSOR 2VGA/RCA/S-VIDEO PRATA",
-        preco: 10.4,
-        qtdEstoque: 10,
-        disponivel: "sim",
-        emDestaque: "sim",
-        departamento: {
-            idDepto: 1,
-            nomeDepto: "Adaptadores"
-        }
-    },
-    {
-        idProduto: 5,
-        descricao: "ADAPTADOR CONVERSOR HDMI / RCA FULLHD 1080 BRANCO",
-        preco: 14.0,
-        qtdEstoque: 5,
-        disponivel: "sim",
-        emDestaque: "nao",
-        departamento: {
-            idDepto: 1,
-            nomeDepto: "Adaptadores"
-        }
-    },
-    {
-        idProduto: 6,
-        descricao: "ADAPTADOR EXTENDER / RJ45 BEGE",
-        preco: 1.0,
-        qtdEstoque: 101,
-        disponivel: "sim",
-        emDestaque: "sim",
-        departamento: {
-            idDepto: 1,
-            nomeDepto: "Adaptadores"
-        }
-    },
-    {
-        idProduto: 7,
-        descricao: "ADAPTADOR EXTENDER HDMI / RJ45 30M RX/TX 4K FHD AZUL",
-        preco: 16.5,
-        qtdEstoque: 10,
-        disponivel: "sim",
-        emDestaque: "sim",
-        departamento: {
-            idDepto: 1,
-            nomeDepto: "Adaptadores"
-        }
-    },
-    {
-        idProduto: 8,
-        descricao: "ADAPTADOR EXTENDER HDMI / RJ45 60M",
-        preco: 39.0,
-        qtdEstoque: 120,
-        disponivel: "sim",
-        emDestaque: "sim",
-        departamento: {
-            idDepto: 1,
-            nomeDepto: "Adaptadores"
-        }
-    },
-    {
-        idProduto: 9,
-        descricao: "ADAPTADOR GRAVADOR CADDY NB 9.5MM / HD 2.5 SATA",
-        preco: 9.5,
-        qtdEstoque: 21,
-        disponivel: "sim",
-        emDestaque: "nao",
-        departamento: {
-            idDepto: 1,
-            nomeDepto: "Adaptadores"
-        }
-    },
-    {
-        idProduto: 10,
-        descricao: "ADAPTADOR HDMI FEMEA / DVI MACHO",
-        preco: 4.0,
-        qtdEstoque: 21,
-        disponivel: "sim",
-        emDestaque: "sim",
-        departamento: {
-            idDepto: 1,
-            nomeDepto: "Adaptadores"
-        }
-    }
-]
+app.use(express.json());
 
-//criando um serviço com método get, caminho path é o /produtos, temos os dados de req e res como parâmetros e vamos responder com a lista de produtos.
+
+//DESAFIO 1 - GET GERAL - OK
 app.get('/produto', (req, res) => {
-    let produtos = listaProdutos
-    res.send(produtos)
+    let produto = listaProdutos
+    res.send(produto)
 })
 
-//criando um serviço com método get, caminho path é o /produtos/:id. Este path significa que vai buscar um id específico quando for digitar a url. Ex.: http://localhost:3000/produto/15
-//abaixo, declaramos a variável id que vai verificar o id digitando pelo usuário. O for vai fazer um looping na lista de produtos até o final. O if vai verificar se o ID digitado pertence a lista de produtos. Se sim, acrescenta 1 no "produtocorreto" e me retorna o produto. Se não, mantém o "produtocorreto" como 0 e retorna a mensagem de erro.
 
+
+
+
+
+//DESAFIO 2 - GET POR ID - OK
 app.get('/produto/:id', (req, res) => {
-    let id = req.params.id;
+    let {id} = req.params;
     let produtocorreto = 0;
     for (listaBusca = 0; listaBusca < listaProdutos.length; listaBusca++){
         if (id == listaProdutos[listaBusca].idProduto){
             produtocorreto = 1;
-            res.status(200).send(listaProdutos[listaBusca]);
+            res.send(listaProdutos[listaBusca]);
             break;
         }
     }
     if (produtocorreto == 0){
-        res.sendStatus(404)
-        // res.status(404).json({message: 'Produto não encontrado. Verifique o ID e tente novamente.'})
+        // res.sendStatus(404)
+        res.status(404).json({message: 'Produto não encontrado. Verifique o ID e tente novamente.'})
     }
 })
 
+
+
+
+
+//DESAFIO 3 POST - INCOMPLETO
 app.post('/produto', (req, res) => {
-    let produto = req.body
+    let {idProduto, descricao, preco, qtdEstoque, disponivel, emDestaque, departamento, idDepto, nomeDepto} = req.body
 
-    if(produto.idProduto < 0 || !produto.descricao || !produto.disponivel || produto.preco <= 0){
-        //res.sendStatus(400)
-        res.status(400).json({message: 'Parâmetros incorretos. Verifique as informações inseridas e tente novamente.'})
+    if (idProduto < 0 || !descricao || preco < 0 || qtdEstoque < 0 || !disponivel || !emDestaque || !departamento){
+        res.sendStatus(400);
     }
-    else{
-        listaProdutos.push(produto);
-        //res.sendStatus(200)
-        res.status(200).json({message: 'Produto incluído na base de dados.'})
-    }
+
+    let novoProduto = {
+        idProduto,
+        descricao,
+        preco,
+        qtdEstoque,
+        disponivel,
+        emDestaque,
+        departamento,
+        idDepto,
+        nomeDepto
+    };
+
+    listaProdutos.push(novoProduto)
+
+    res.json(novoProduto)
+    
+
+    // if(produto.idProduto < 0 || !produto.descricao || !produto.disponivel || produto.preco <= 0){
+    //     //res.sendStatus(400)
+    //     res.status(400).json({message: 'Parâmetros incorretos. Verifique as informações inseridas e tente novamente.'})
+    // }
+    // else{
+    //     listaProdutos.push(produto);
+    //     //res.sendStatus(200)
+    //     res.status(200).json({message: 'Produto incluído na base de dados.'})
+    // }
 })
+
+
+//DESAFIO 3 ISIDRO
+// app.post("/produto", (req, res) => {
+//     let produto = req.body;
+    
+//     if (produto.codProduto < 0 || !produto.descricao || !produto.disponivel || produto.preco <= 0 ){
+//         res.sendStatus(400);
+//     }
+//     else{
+//         listaProdutos.push(produto);
+//         res.sendStatus(200);
+//     }
+    
+// })
+
+// app.listen(3000, () => {
+//     console.log("Servidor no ar na porta 3000 - vamos ver se funfa!!!");
+// })
+
+
+
+//DESAFIO 4 PUT - INCOMPLETO
+app.put('/produto/:id', (req, res) => {
+    let {id} = req.params;
+    let produto = listaProdutos.find(prod => prod.id == id);
+    if (!produto) return res.status(204).json();
+
+    let {preco} = req.body;
+    produto.preco = preco,
+    res.json(produto);
+})
+
+
+
+
+
+
+
+
+
+//DESAFIO 5 GET DEPARTAMENTO - INCOMPLETO
+app.get('/departamento', (req, res) => {
+    let produto = listaProdutos.departamento
+    res.send(produto)
+})
+
+
+
+
+
+
+
+
+
+//DESAFIO 6 GET ID DEPARTAMENTO - INCOMPLETO
 
 
 
 //subindo o servidor node.
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Server is running`);
+});
